ed9446ce45e109e82587dac34f0c0fb338ce1d0c
brian-nunez <bnunez9999@gmail.com>
2026-03-10 20:54:48 -0700

    feat(format): add templ support

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -9,6 +9,7 @@ return {
       },
       formatters_by_ft = {
         lua = { 'stylua' },
+        templ = { 'templ' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
